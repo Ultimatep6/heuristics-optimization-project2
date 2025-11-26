dc65f2f41d722809eca7d1bd25efeb8c0f18729f
Matteo bekink <matteo.bekink@gmail.com>
2025-11-26 12:13:55 +0100

    Final grafo and gitignore

--- parte_2/grafo_v2.py
+++ parte_2/grafo_v2.py
@@ -6,15 +6,13 @@ script_dir = os.path.dirname(__file__)
 
 def load_nodes():
     nodeDict = {}
-    node_dir = os.path.join(script_dir, '../data/USA-road-d.BAY.co')
+    node_dir = os.path.join(script_dir, r'./data/USA-road-d.BAY.co')
 
-    with os.open(node_dir, 'r') as node_file:
-        # find the first line with 'v' to get the index of the first node
-        for line in node_file:
-            if line.startswith('v'):
-                first_node_index = int(line.split()[1])
-                break
-        for node in node_file[first_node_index:]:
+    with open(node_dir, 'r') as node_file:        
+        # only read lines starting from first_node_index
+        for i, node in enumerate(node_file):
+            if not node.startswith('v'):
+                continue
             parts = node.split()
             node_id = int(parts[1])
             long = int(parts[2])
@@ -25,14 +23,12 @@ def load_nodes():
 
 def load_paths():
     pathDict = {}
-    path_dir = os.path.join(script_dir, '../data/USA-road-d.BAY.gr')
+    path_dir = os.path.join(script_dir, r'./data/USA-road-d.BAY.gr')
 
-    with os.open(path_dir, 'r') as path_file:
-        for line in path_file:
-            if line.startswith('a'):
-                first_node_index = int(line.split()[1])
-                break
-        for path in path_file[first_node_index:]:
+    with open(path_dir, 'r') as path_file:
+        for i, path in enumerate(path_file):
+            if not path.startswith('a'):
+                continue
             parts = path.split()
             from_node = int(parts[1])
             to_node = int(parts[2])
@@ -42,15 +38,15 @@ def load_paths():
                 pathDict[from_node].append(Path(from_node, to_node, cost))
             else:
                 pathDict[from_node] = [Path(from_node, to_node, cost)]
+
     return pathDict
 
 
-if __name__ == "__main__":
-    print("Loading nodes...")
-    nodes = load_nodes()
-    print(f"Loaded {len(nodes)} nodes.\n")
+# if __name__ == "__main__":
+#     print("Loading nodes...")
+#     nodes = load_nodes()
+#     print(f"Loaded {len(nodes.keys())} nodes.\n")
 
-
-    print("Loading paths...")
-    paths = load_paths()
-    print(f"Loaded {len(paths)} paths.")
+#     print("Loading paths...")
+#     paths = load_paths()
+#     print(f"Loaded {len(paths.keys())} paths.")
